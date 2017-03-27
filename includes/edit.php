70e55879ca46ecd1676029f9a234c179940a4ba5
Marcus <marcus.gbrl@live.com>
2017-03-26 20:13:16 -0400

    tirando o form

--- includes/edit.php
+++ includes/edit.php
@@ -1,9 +1,7 @@
 
 <div class='col-md-8'>
-    <form>
         <input type="hidden" id='editId' value="<?=$_GET['id']?>"/>
         Nome: <input type="text" id='editNome' value="<?=$_GET['nome']?>"/><br>
         Email: <input type="text" id='editEmail' value="<?=$_GET['email']?>"/><br>
         <button id='editButton'>Salvar</button>
-    </form>    
 </div>
